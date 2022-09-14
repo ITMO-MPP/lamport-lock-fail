82975b5534298205b9e416cd6b49cdb1059771e3
Roman Elizarov <elizarov@gmail.com>
2022-09-14 19:51:51 +0300

    2022 version

--- src/Verifier.kt
+++ src/Verifier.kt
@@ -1,20 +1,19 @@
-package ru.ifmo.pp.lamport_lock
-
 import java.io.BufferedReader
 import java.io.FileReader
 import java.util.Collections.synchronizedList
-import java.util.stream.*
+import kotlin.system.*
 
-fun main(args: Array<String>) {
-    val t1 = LockThread(0)
-    val t2 = LockThread(1)
+fun main() {
+    val execution = Execution("solution.txt")
+    val t1 = LockThread(0, execution)
+    val t2 = LockThread(1, execution)
     t1.start()
     t2.start()
     t1.join()
     t2.join()
     if (acquired != 2) {
         println("Wrong execution: only $acquired threads entered in critical section")
-        System.exit(1)
+        exitProcess(1)
     }
     println("Correct example")
 }
@@ -22,9 +21,9 @@ fun main(args: Array<String>) {
 var error = false
 var acquired = 0
 
-class LockThread(val id: Int) : Thread() {
+class LockThread(private val id: Int, private val execution: Execution) : Thread() {
     override fun run() {
-        lock(id)
+        execution.lock(id)
         acquired++
     }
 }
@@ -32,20 +31,20 @@ class LockThread(val id: Int) : Thread() {
 val label = arrayOf(0, 0)
 val N = label.size
 
-fun lock(id: Int) {
-    var my = 1;
-    for (k in 0..N - 1) if (k != id) {
+fun Execution.lock(id: Int) {
+    var my = 1
+    for (k in 0 until N) if (k != id) {
         waitForRead(id, "label[$k]")
-        val l = label[k];
+        val l = label[k]
         checkRead(l.toString())
         my = maxOf(my, l + 1)
     }
     waitForWrite(id, "label[$id]", my.toString())
     label[id] = my
-    for (k in 0..N - 1) if (k != id) {
+    for (k in 0 until N) if (k != id) {
         while (true) {
             waitForRead(id, "label[$k]")
-            val other = label[k];
+            val other = label[k]
             checkRead(other.toString())
             if (other == 0 || other > my || (other == my && k > id))
                 break
@@ -53,21 +52,19 @@ fun lock(id: Int) {
     }
 }
 
-private val execution: MutableList<Action> = synchronizedList(parseExecution("execution"));
-
-fun waitForRead(id: Int, loc: String) {
+fun Execution.waitForRead(id: Int, loc: String) {
     waitForCurThreadAction(id)
     checkLocation(id, loc)
     println("Start read operation by thread #$id from $loc")
 }
 
-fun checkRead(res: String) {
+fun Execution.checkRead(res: String) {
     checkValue(res)
     println("Read operation completed, result=$res")
     execution.removeAt(0)
 }
 
-fun waitForWrite(id: Int, loc: String, value: String) {
+fun Execution.waitForWrite(id: Int, loc: String, value: String) {
     waitForCurThreadAction(id)
     checkLocation(id, loc)
     checkValue(value)
@@ -75,27 +72,27 @@ fun waitForWrite(id: Int, loc: String, value: String) {
     execution.removeAt(0)
 }
 
-private fun waitForCurThreadAction(id: Int) {
+private fun Execution.waitForCurThreadAction(id: Int) {
     try {
-        while (execution[0].tid != id);
+        while (execution[0].tid != id) { /* spin */ }
     } catch (e: IndexOutOfBoundsException) {
         println("Performing action in thread #$id, but no more actions in execution")
-        error = true;
-        System.exit(1);
+        error = true
+        exitProcess(1)
     }
 }
 
-private fun checkLocation(id: Int, loc: String) {
+private fun Execution.checkLocation(id: Int, loc: String) {
     if (execution[0].loc != loc) {
         println("$id, Invalid location, cur=$loc, expected_action=${execution[0]}")
-        System.exit(1);
+        exitProcess(1)
     }
 }
 
-private fun checkValue(value: String) {
+private fun Execution.checkValue(value: String) {
     if (execution[0].value != value) {
         println("Invalid value, cur=$value, expected_action=${execution[0]}")
-        System.exit(1);
+        exitProcess(1)
     }
 }
 
@@ -109,7 +106,7 @@ data class Action(
 enum class ExType { READ, WRITE }
 
 private fun parseAction(s: String): Action {
-    val parts = s.trim().split(" ");
+    val parts = s.trim().split(" ")
     return Action(tid = parts[0].toInt(),
             type = if (parts[2] == "rd") ExType.READ else ExType.WRITE,
             loc = parts[3],
@@ -118,6 +115,16 @@ private fun parseAction(s: String): Action {
 
 private fun parseExecution(filename: String): List<Action> {
     BufferedReader(FileReader(filename)).use { br ->
-        return br.lines().map(::parseAction).collect(Collectors.toList())
+        val lines = br.readLines()
+            .map { it.substringBefore('#').trim() }
+            .filter { it.isNotEmpty() }
+        val author = lines[0]
+        require(author != "<Last-Name> <First-Name>") { "The first line must be filled in with name" }
+        println("Validating $filename: $author")
+        return lines.drop(1).map { parseAction(it) }
     }
+}
+
+class Execution(filename: String) {
+    val execution: MutableList<Action> = synchronizedList(parseExecution(filename))
 }
